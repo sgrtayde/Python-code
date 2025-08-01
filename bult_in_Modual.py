03aff03325a8ce7166c6f5db09c23f2ff3968c7c
Sagar tayde <sagartayde201@gmail.com>
2025-08-01 10:20:46 +0530

    Python code
    All python code

---
+++ bult_in_Modual.py
@@ -0,0 +1,19 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(""" 
+
+Twinkle, twinkle, little star,  
+How I wonder what you are!  
+Up above the world so high,  
+Like a diamond in the sky.
+
+When the blazing sun is gone,  
+When he nothing shines upon,  
+Then you show your little light,  
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark  
+Thanks you for your tiny spark.  
+He could not see which way to go,  
+If you did not twinkle so. """)
+engine.runAndWait()
