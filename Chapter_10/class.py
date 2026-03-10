3d514f75b425259821fb854a50e0f191a7267e93
Sagar tayde <sagartayde201@gmail.com>
2026-03-10 21:48:34 +0530

    Add files via upload

---
+++ Chapter_10/class.py
@@ -0,0 +1,12 @@
+class employee:
+    language = "py"  # This is a class attribut
+    salary = 1200000
+
+sagar = employee()
+sagar.name = "sagar tayde" # This is a instance attribuat
+print(sagar.name,sagar.language,sagar.salary)
+
+harshal = employee()
+harshal.name = "harshal kale"
+print(harshal.name,harshal.salary,harshal.language)
+   
